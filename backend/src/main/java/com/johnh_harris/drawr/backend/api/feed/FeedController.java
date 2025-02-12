ea48f62135301d523fb9d36bc2e75bafe572587a
John <39563774+johnh-harris@users.noreply.github.com>
2025-02-11 21:57:59 -0500

    Started HTML page load

--- backend/src/main/java/com/johnh_harris/drawr/backend/api/feed/FeedController.java
+++ backend/src/main/java/com/johnh_harris/drawr/backend/api/feed/FeedController.java
@@ -1,8 +1,10 @@
 package com.johnh_harris.drawr.backend.api.feed;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +16,33 @@ import java.util.List;
  * which in turn makes calls to the database asking for a list
  * of Feed objects (images)
  */
-@RestController
+@Controller
 public class FeedController {
 
     @Autowired
     FeedRepository feedRepository;
 
-    @GetMapping("/")
-    public ResponseEntity<List<Feed>> homePageFeed() {
-        System.out.println("Grabbing feed data");
+    /*@GetMapping("/")
+    public ResponseEntity<List<Feed>> homePageFeed(HttpServletRequest request) {
+        // Use this to check banned IP addresses
+        //String ip = request.getRemoteAddr();
+        System.out.println("Grabbing feed data\nIP is: " + request.getRemoteAddr());
         List<Feed> feed = feedRepository.findByRange("");
         return new ResponseEntity<>(feed, HttpStatus.OK);
+    }*/
+
+    @GetMapping("/")
+    public String homePageFeed() {
+        /*
+        TODO:
+         - check if IP isn't banned
+         - then return a feed
+
+         */
+        // Use this to check banned IP addresses
+        //String ip = request.getRemoteAddr();
+        System.out.println("Grabbing feed data");
+        //List<Feed> feed = feedRepository.findByRange("");
+        return "index";
     }
 }
